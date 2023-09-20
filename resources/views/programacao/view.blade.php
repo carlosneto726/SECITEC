c2a30d6f3cac38ee681e3fef8acb84f839917849
Barro23 <pb948697@gmail.com>
2023-09-20 19:06:33 -0300

    test de novo

---
+++ resources/views/programacao/view.blade.php
@@ -0,0 +1,8 @@
+@extends('templates.template')
+@section('content')
+
+<section>
+    <h1>PROGRAMACAO</h1>
+</section>
+
+@endsection
---
+++ resources/views/sobre/view.blade.php
@@ -0,0 +1,31 @@
+@extends('templates.template')
+@section('content')
+
+<section class="about section-padding" id="section_2">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-10 col-12">
+                <h2 class="mb-4">Sobre <u class="text-info">o evento</u></h2>
+            </div>
+
+            <div class="col-lg-6 col-12">
+                <h3 class="mb-3">SUBTITULO</h3>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            </div>
+            <div class="col-lg-6 col-12">
+                ADICIONAR ALGO AQUI (FOTOS ETC)
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+@endsection
