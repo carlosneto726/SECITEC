8ce3948ea19a745be043d58b0c81165fb1fa41eb
Barro23 <pb948697@gmail.com>
2023-09-20 21:31:46 -0300

    programacao com conecxao com o banco

---
+++ public/js/programacao.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+
+
+    for (let i = 0; i < programacoes.length; i++) {
+        var horai = programacoes[i]['horarioI'];
+        var horaf = programacoes[i]['horarioF'];
+        
+       
+
+        document.getElementById("programacoes").innerHTML += 
+        
+    '<div class="col-lg-8 col-12 mt-3 mt-lg-0">' +
+                                                
+                                            
+    '     <h4 class="mb-2"> '+programacoes[i]['titulo']+'</h4>' +
+    '     <p>'+programacoes[i]['descricao']+'</p>' +
+    '     <div class="d-flex align-items-center mt-4">' +
+    '         <div class="avatar-group d-flex">' +
+    '             <img src="'+programacoes[i]['url_p']+'" class="img-fluid avatar-image" class="img-fluid avatar-image" alt="">' +
+    '             <div class="ms-3">' +
+                    programacoes[i]['palestrante']+
+    '                 <p class="speakers-text mb-0">'+programacoes[i]['titulacao']+'</p>' +
+    '             </div>' +
+    '         </div>' +
+    '         <span class="mx-3 mx-lg-5">' +
+    '             <i class="bi-clock me-2"></i>' +
+    '              '+horai+' - '+horaf+' ' +
+    '         </span>' +
+    '         <span class="mx-1 mx-lg-5">' +
+    '             <i class="bi-layout-sidebar me-2"></i>' +
+                    programacoes[i]['local']+
+    '         </span>' +
+    '     </div>' +
+    '   </div>' +
+    ' </div>' +
+    ' <div class="row border-bottom pb-5 mb-5">';
+    }
+}
