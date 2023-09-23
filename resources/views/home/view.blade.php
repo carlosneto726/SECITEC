82aa6308021349ede7ef60b492661bbe370aa200
Juliano Marcelo Schaurich Júnior <schaurich.jmsj@gmail.com>
2023-09-23 15:42:35 -0300

    estilizacao pagina home e cores

--- resources/views/home/view.blade.php
+++ resources/views/home/view.blade.php
@@ -6,17 +6,41 @@
                     <div class="row">
 
                         <div class="col-lg-5 col-12 m-auto">
-                            <div class="hero-text">
+                            <div class="hero-text text-light">
+                                <p class="fs-1 fw-semibold text-reset" >SECITEC 2023</p>
+                                <p class="fs-2 fw-semibold text-reset" >IFG, Campus Formosa</p>
 
-                                <h1 class="text-white mb-4"><u class="text-info">SECITEC</u> <br> IFG, 2023</h1>
+                                <p class="fs-1 fw-bolder text-reset" id="countdown"></p>
 
-                                <div class="d-flex justify-content-center align-items-center">
+                                <script>
+                                    // Define a data alvo (30 de setembro de 2023)
+                                    const targetDate = new Date('2023-10-23T00:00:00').getTime();
+
+                                    // Atualiza o cronômetro a cada segundo
+                                    const countdown = setInterval(function() {
+                                        const currentDate = new Date().getTime();
+                                        const timeLeft = targetDate - currentDate;
+
+                                        if (timeLeft <= 0) {
+                                            clearInterval(countdown);
+                                            document.getElementById('countdown').innerHTML = 'Tempo esgotado!';
+                                        } else {
+                                            const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+                                            const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+                                            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+                                            document.getElementById('countdown').innerHTML = `
+                                                ${days}:${hours}:${minutes}:${seconds}
+                                            `;
+                                        }
+                                    }, 1000); // Atualiza a cada 1 segundo
+                                </script>
+
+                                <div class="d-flex justify-content-center align-items-center p-3">
                                     <span class="date-text">06 a 09 de março, 2024</span>
-
-                                    <span class="location-text">IFG, Formosa</span>
                                 </div>
 
-                                <a href="#section_2" class="custom-link bi-arrow-down arrow-icon"></a>
                             </div>
                         </div>
                     </div>
@@ -27,8 +51,9 @@
                     </video>
                 </div>
             </section>
-
-
+            
+            
+            <!--
             <section class="highlight">
                 <div class="container">
                     <div class="row">
@@ -63,6 +88,8 @@
 
                     </div>
                 </div>
+            </section>
+            -->
 </section>
 
 
