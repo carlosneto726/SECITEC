298d5206821b9f7e8bd06f16c6666917a1ffa4c8
carlosneto726 <carlosneto726@gmail.com>
2023-09-23 20:44:19 -0300

    adicionando novas rotas

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,13 @@ Route::post('/admin/eventos/alterar', [AdministradorController::class, 'updateEv
 Route::post('/admin/eventos/deletar', [AdministradorController::class, 'deleteEvento']);
 Route::get('/admin/proponente', [AdministradorController::class, 'viewProponente']);
 Route::post('/admin/proponente/cadastrar', [AdministradorController::class, 'insertProponente']);
+Route::put('/admin/proponente/atualizar/{id_proponente}', [AdministradorController::class, 'updateProponente']);
 Route::post('/admin/proponente/deletar', [AdministradorController::class, 'deleteProponente']);
+Route::get('/admin/presenca/{id_evento}', [AdministradorController::class, 'viewPresenca']);
+Route::post('/admin/presenca/checkin', [AdministradorController::class, 'checkin']);
+Route::post('/admin/presenca/checkout', [AdministradorController::class, 'checkout']);
+
+Route::get('/teste', [AdministradorController::class, 'enviarEmail']);
 
 Route::get('/loginUser', [Controller::class, 'viewLogin']);
 Route::get('/cadastrarUser', [Controller::class, 'viewCadastrar']);
