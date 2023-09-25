75330367c727e5090134a784cacbc8f1b2e8e00d
maykkk1 <maycon5591@gmail.com>
2023-09-25 14:53:10 -0300

    Salvando alteracoes

--- resources/views/usuarios/home.blade.php
+++ resources/views/usuarios/home.blade.php
@@ -1,22 +1,29 @@
 @extends('templates.template')
 @section('content')
     <div class="container">
-        <h2>Bem vindo, {{ $usuario->nome }}</h2>
+        <h2 class="bem-vindo-user">Bem vindo, {{ $usuario->nome }}</h2>
         <hr>
         <section class="user-eventos container row">
             @foreach ($eventosMapeados as $evento)
-                <div class="event-card col-lg-6">
-                    <div class="event-content col-8">
+                <div class="event-card col-12">
+                    <div class="event-content col-lg-10 col-8">
                         <div class="text">
                             <h3>{{ $evento->titulo }}</h3>
                             <p>{{ $evento->descricao }}</p>
                         </div>
                         <hr>
-                        <button id="{{ $evento->id }}" onclick="enviarRequisicao({{ $evento->id }})" type="button"
-                            class="btn {{ $evento->usuario_cadastrado ? 'btn-danger' : 'btn-success' }}">{{ $evento->usuario_cadastrado ? 'Descadastrar' : 'Cadastrar' }}</button>
+                        <div class="row">
+                            <div class="col-6">
+                                <button id="{{ $evento->id }}" onclick="enviarRequisicao({{ $evento->id }})" type="button"
+                                    class="btn {{ $evento->usuario_cadastrado ? 'btn-danger' : 'btn-success' }}">{{ $evento->usuario_cadastrado ? 'Descadastrar' : 'Cadastrar' }}</button>
+                            </div>
+                            <div class="col-6 tipo-evento-wrapper">
+                                <span class="tipo-evento">{{ $evento->tipo_evento_nome }}</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="event-image col-4">
-                        <img src="" alt="">
+                    <div class="event-image col-lg-2 col-4">
+                        <img src="{{ $evento->url }}" class="img-fluid" alt="Responsive image">
                     </div>
                 </div>
             @endforeach
