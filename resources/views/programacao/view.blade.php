0a75290a09f9c432658ca7dc33a7db449579454a
Barro23 <pb948697@gmail.com>
2023-09-25 16:39:37 -0300

    programacao ordenada por hora

--- resources/views/programacao/view.blade.php
+++ resources/views/programacao/view.blade.php
@@ -14,9 +14,60 @@
         <div class="row">
             <div class="col-lg-12 col-12">
                 <h2 class="mb-5 text-center">Nossa <u class="text-success">Programação</u></h2>
+                
                 <div class="tab-content mt-5" id="nav-tabContent">
                     <div class="tab-pane fade show active" id="nav-DayOne" role="tabpanel" aria-labelledby="nav-DayOne-tab">
-                        <div class="row border-bottom pb-5 mb-5">                                 
+                        <div class="row border-bottom pb-5 mb-5">  
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                      Dia 23/10/2023
+                                    </button>
+                                  </h2>
+                                  <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                      <strong id="programacoes23"></strong>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                      Dia 24/10/2023
+                                    </button>
+                                  </h2>
+                                  <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                      <strong id="programacoes24"></strong> 
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                      Dia 25/10/2023
+                                    </button>
+                                  </h2>
+                                  <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                      <strong id="programacoes25"></strong> 
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+                                      Dia 26/10/2023
+                                    </button>
+                                  </h2>
+                                  <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                      <strong id="programacoes26"></strong> 
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>                               
                             <div class="row border-bottom pb-5 mb-5" id="programacoes">     
                                                  
                             </div>
