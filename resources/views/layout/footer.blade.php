6f3cddbf93d424b4a05e50acf0744ce8eadc1b3e
Barro23 <pb948697@gmail.com>
2023-09-26 20:32:18 -0300

    programacao sem o hash

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -20,21 +20,27 @@
           <!--Grid column-->
 
           <!--Grid column-->
+          
           <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-            </svg>
-            <p class="fs-5 pt-3">secitec2023@gmail.com</p>
+              <a href="mailto:secitec2023@gmail.com">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+              </svg>
+              <p class="fs-5 pt-3">secitec2023@gmail.com</p>
+            </a>
           </div>
+          
           <!--Grid column-->
 
           <!--Grid column-->
           <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-            </svg>
-            <p class="fs-5 pt-3">Rua 64 - Parque Lago, Formosa - GO</p>
-            <p class="fs-5">73813-816</p>
+            <a href="https://www.google.com/maps/place/IFG+-+C%C3%A2mpus+Formosa/@-15.562847,-47.327808,10z/data=!4m6!3m5!1s0x9350a2f73fd3c36f:0xb8ffbb72c45a6bd6!8m2!3d-15.5628475!4d-47.3278082!16s%2Fg%2F11bbrlt0dm?hl=pt-BR&entry=ttu">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+              </svg>
+              <p class="fs-5 pt-3">Rua 64 - Parque Lago, Formosa - GO</p>
+              <p class="fs-5">73813-816</p>
+              </a>
           </div>
           <!--Grid column-->
         </div>
