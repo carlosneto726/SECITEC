76da3471f7fd40099b9fd2fda30610bffeb393a1
carlosneto726 <carlosneto726@gmail.com>
2023-09-28 20:12:40 -0300

    concertando o bug de cache

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}">
-    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap-icons.css")}}">
-    <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}">
-    <link rel="stylesheet" href="{{asset('css\user.css')}}">
-    <link rel="stylesheet" href="{{asset("css\app.css")}}">
+    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}?v=1.0">
+    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap-icons.css")}}?v=1.0">
+    <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}?v=1.0">
+    <link rel="stylesheet" href="{{asset('css\user.css')}}?v=1.0">
+    <link rel="stylesheet" href="{{asset("css\app.css")}}?v=1.0">
     <title>SECITEC</title>
 </head>
 <body>
@@ -27,10 +27,10 @@
         @include('layout.footer')
     </footer>
 
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.sticky.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/usuario.js') }}"></script>
+    <script src="{{asset('js/jquery.min.js')}}?v=1.0"></script>
+    <script src="{{asset('js/jquery.sticky.js')}}?v=1.0"></script>
+    <script src="{{asset('js/app.js')}}?v=1.0"></script>
+    <script src="{{asset('js/usuario.js')}}?v=1.0"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script>
         var alerta = document.getElementById("alert");
