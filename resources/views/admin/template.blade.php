d071a87147326199dfceb300694cffad48264e31
carlosneto726 <carlosneto726@gmail.com>
2023-10-01 14:37:27 -0300

    atualizando a versão do css e js

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{asset("css\app.css")}}">
-    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}">
-    <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}">
-    <link rel="stylesheet" href="{{asset("css\app.css")}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}?v=2.0">
+    <link rel="stylesheet" href="{{asset("css\app.css")}}?v=2.0">
+    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}?v=2.0">
+    <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}?v=2.0">
+    <link rel="stylesheet" href="{{asset("css\app.css")}}?v=2.0">
     <title>SECITEC</title>
 </head>
 
