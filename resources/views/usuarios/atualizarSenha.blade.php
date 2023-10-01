22cd5f1a4bfdf8c2efa6f2b09136eebebbf298e2
Juliano Marcelo Schaurich Júnior <schaurich.jmsj@gmail.com>
2023-10-01 16:08:40 -0300

    home - att botao sair e img hackaton; atualizar-senha - att

--- resources/views/usuarios/atualizarSenha.blade.php
+++ resources/views/usuarios/atualizarSenha.blade.php
@@ -1,10 +1,10 @@
 @extends('templates.template')
 @section('content')
 
-<h2 class="text-center">Redefinir senha</h2>
+<div class="container section-padding"> 
 
-<div class="p-5 mx-auto container rounded" style="width: fit-content; background-color: var(--light-gray);">
-    <h4>Redefina a sua senha</h4> 
+<div class="p-5 border mx-auto card border-0 shadow rounded-3 my-5" style="width: fit-content; background-color: var(--light-gray);">
+    <h4>Redefir <u class="text-success">senha</u></h4>
 
     <form method="POST" action="{{url("/redefinir-senha/".$token)}}">
         @csrf
@@ -15,7 +15,8 @@
         </div>
 
         <button class="btn btn-success mt-2 w-100" type="submit">Atualizar senha</button>
+        <hr class="my-4">
     </form>
 </div>
-
+</div>
 @endsection
