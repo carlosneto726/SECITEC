1ad5f6da3bd0e4ed9602093d1cb6375184f21795
carlosneto726 <carlosneto726@gmail.com>
2023-10-02 11:41:57 -0300

    new update

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Icone da aba do navegador
+    <link rel="icon" href="{//{asset('')}}">
+    -->
     <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}?v=1.0">
     <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap-icons.css")}}?v=1.0">
     <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}?v=1.0">
