ad80d2df21a1f3b5687948a808bcb767a4a50c9b
Juliano Marcelo Schaurich Júnior <schaurich.jmsj@gmail.com>
2023-10-03 14:44:59 -0300

    programacao - corrigido

--- resources/views/proponente/view.blade.php
+++ resources/views/proponente/view.blade.php
@@ -2,15 +2,28 @@
 @extends('templates.template')
 @section('content')
 
+<section class="container section-padding">
 
-<h1>NOME: {{$proponente->nome}}</h1>
-<h3>TITULAção: {{$proponente->titulacao}}</h3>
+<div class="row">
+    <div class="col-2">
+        <img class="img-fluid rounded-circle" src="{{asset($proponente->url)}}">
+    </div>
+</div>
+<div class="row">
+    <div class="img-wrapper-proponente col-6">
+        <h1>NOME: {{$proponente->nome}}</h1>
+        <h3>TITULAção: {{$proponente->titulacao}}</h3>
+    </div>
+</div>
+
+
+
+
 <h3>rede1: {{$proponente->redes->rede1}}</h3>
 <h3>rede2: {{$proponente->redes->rede2}}</h3>
 <h3>rede3: {{$proponente->redes->rede3}}</h3>
 
-img proponente
-<img src="{{asset($proponente->url)}}">
+
 
 
 <h1>Eventos que o proponente participa</h1>
@@ -28,4 +41,5 @@ img proponente
 
 {{var_dump($proponente)}}
 {{var_dump($eventos)}}
+</section>
 @endsection
