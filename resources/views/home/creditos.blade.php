5de9ac17c1b098546ad08e55fa3f997121a6a0ff
Barro23 <pb948697@gmail.com>
2023-10-07 12:28:32 -0300

    creditos

--- resources/views/home/creditos.blade.php
+++ resources/views/home/creditos.blade.php
@@ -20,52 +20,74 @@
         
         <p class="fs-2 fw-bold border-bottom pt-5 pb-2 mb-0"><u class="text-success">Desenvolvedores</u></p>
         <a href="https://github.com/carlosneto726" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="https://avatars.githubusercontent.com/u/85174500?s=400&u=b24077b26834b9c99aa5878b4169e48ab4d97b45&v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://avatars.githubusercontent.com/u/85174500?s=400&u=b24077b26834b9c99aa5878b4169e48ab4d97b45&v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" style="object-fit: cover" width="64" height="64">
             <p class="pb-3 mb-0 small lh-sm ">
                 <strong class="fw-bold d-block text-gray-dark">Carlos Henrique Neto</strong>
                 Desenvolvedor Web FullStack, engenheiro de software, Analista e graduando em TADS.
             </p>
         </a>
         <a href="https://github.com/JulianoSchaurich" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="https://julianoschaurich.github.io/images/foto-perfil.png" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://julianoschaurich.github.io/images/foto-perfil.png" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
             <p class="pb-3 mb-0 small lh-sm">
                 <strong class="fw-bold d-block text-gray-dark">Juliano Schaurich</strong>
                 Graduando em Análise e Desenvolvimento de Sistemas e Técnico em Informática.
             </p>
         </a>
         <a href="https://github.com/maykkk1" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="https://maycondouglas.netlify.app/images/profile.jpg" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://maycondouglas.netlify.app/images/profile.jpg" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
             <p class="pb-3 mb-0 small lh-sm">
                 <strong class="fw-bold d-block text-gray-dark">Maycon Douglas</strong>
-                Desenvolvedor FullStack Graduando em TADS.
+                Fullstack Developer.
             </p>
         </a>
         <a href="https://github.com/Barro23" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="https://avatars.githubusercontent.com/u/102668293?v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://avatars.githubusercontent.com/u/102668293?v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
             <p class="pb-3 mb-0 small lh-sm">
                 <strong class="fw-bold d-block text-gray-dark">Pedro Henrique Barros</strong>
-                Desenvolvedor web FullStack, Editor de vídeo, Editor de Fotos graduando em TADS.
+                Desenvolvedor web FullStack graduando em TADS, Editor de vídeo e Editor de Imagens. 
             </p>
         </a>
         <a href="https://github.com/matheus341" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="{{asset('storage/images/avatar/placeholder.jpeg')}}" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://media.licdn.com/dms/image/C4D03AQHgQUdiAazG2w/profile-displayphoto-shrink_800_800/0/1661025269121?e=1701907200&v=beta&t=yePJyNzxisFmi5E3Vvawiii8ARe7hfq8nht1_viuuYU" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
             <p class="pb-3 mb-0 small lh-sm">
                 <strong class="fw-bold d-block text-gray-dark">Matheus Rodrigues</strong>
                 Ciência de Dados, SQL, Power BI, Cloud e graduando em TADS.
             </p>
         </a>
         <a href="https://github.com/OtavioProfeta" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="{{asset('storage/images/avatar/placeholder.jpeg')}}" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://media.licdn.com/dms/image/D5635AQGao0mFUfDA9g/profile-framedphoto-shrink_800_800/0/1694139134493?e=1697299200&v=beta&t=Wi26n1kMCXGhlwCRysthqXke-d2CNViIuBO4Dxb3Re8" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
             <p class="pb-3 mb-0 small lh-sm">
                 <strong class="fw-bold d-block text-gray-dark">Otávio Profeta</strong>
                 Desenvolvimento de sistemas HTML, CSS, Bootstrap, PHP, C#, UX/UI e graduado em TADS.
             </p>
         </a>
+        <a href="https://github.com/Daniela183" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
+            <img src="https://avatars.githubusercontent.com/u/77057567?v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
+            <p class="pb-3 mb-0 small lh-sm">
+                <strong class="fw-bold d-block text-gray-dark">Daniela Soares</strong>
+                IFG - Tecnologia em Análise e Desenvolvimento de Sistemas (6/6) e graduada em TADS.
+            </p>
+        </a>
         <a href="https://github.com/joaograndotto" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
-            <img src="https://avatars.githubusercontent.com/u/63874169?v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <img src="https://avatars.githubusercontent.com/u/63874169?v=4" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
             <p class="pb-3 mb-0 small lh-sm">
                 <strong class="fw-bold d-block text-gray-dark">João Gabriel Grandotto</strong>
-                IFG - Tecnologia em Análise e Desenvolvimento de Sistemas (6/6) e graduando em TADS.
+                IFG - Tecnologia em Análise e Desenvolvimento de Sistemas (6/6) e graduado em TADS.
+            </p>
+        </a>
+        
+        <a href=" http://lattes.cnpq.br/1169788371765122" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
+            <img src="https://secitecformosa.online/storage/images/avatar/palestraAfr%C3%A2nio%20Furtado%20de%20Oliveira%20Neto.jpg"  style="object-fit: cover" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64">
+            <p class="pb-3 mb-0 small lh-sm">
+                <strong class="fw-bold d-block text-gray-dark">Afranio Furtado de Oliveira Neto</strong>
+                Mestre em Educação e Tecnologia pela IFTM , e Professor EBTT no Instituto Federal de Educação, Ciência e Tecnologia de Goiás, Campus Formosa.
+            </p>
+        </a>
+        <a href=" http://lattes.cnpq.br/7088277920062488" target="_blank" class="d-flex text-body-secondary pt-3 pb-1 border-bottom">
+            <img src="https://csbc.ufsc.br/wp-content/uploads/2021/07/FMD-Painel-Uyara_FMD.jpg" class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="64" height="64" style="object-fit: cover">
+            <p class="pb-3 mb-0 small lh-sm">
+                <strong class="fw-bold d-block text-gray-dark">Uyara Ferreira Silva</strong>
+                Doutora em Ciência da Computação pela UFG, com mestrado em Engenharia Elétrica e da Computação pela UFG. Professora no IFG-Campus Formosa.
             </p>
         </a>
         <small class="d-block text-end mt-3">
