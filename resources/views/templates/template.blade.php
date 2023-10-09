deacad183eceb5c0e99dabb50d2ae3280e99b05f
carlosneto726 <carlosneto726@gmail.com>
2023-10-09 16:49:02 -0300

    fazendo SEO

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Participe da SECITEC 2023 do IFG-Formosa! Crie sua conta, inscreva-se em eventos, e receba certificados de participação. Esteja pronto para uma experiência educativa e enriquecedora!">
     <link rel="icon" href="{{asset('images\logo.png')}}">
     <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}?v=1.0">
     <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap-icons.css")}}?v=1.0">
