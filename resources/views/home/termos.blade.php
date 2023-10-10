eed64fb64cfc2536d756135ae1d8fc41327538e4
Barro23 <pb948697@gmail.com>
2023-10-10 15:06:11 -0300

    termos arrumado

--- resources/views/home/termos.blade.php
+++ resources/views/home/termos.blade.php
@@ -19,7 +19,7 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">1.Aceitação dos Termos</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Ao acessar e utilizar nosso site, você concorda com todos os termos e condições
                                 estabelecidos nestes Termos de Uso. Se você não concordar com qualquer parte destes Termos, 
                                 não deverá acessar ou utilizar nosso site.
@@ -29,7 +29,7 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">2.Cadastro de Usuário</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Para acessar determinadas funcionalidades do nosso site, você poderá ser solicitado a criar 
                                 uma conta de usuário. Você é responsável por manter a confidencialidade das suas informações de
                                 login e é o único responsável por todas as atividades que ocorrerem em sua conta.
@@ -39,11 +39,11 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">3.Uso de Dados Pessoais</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Ao se cadastrar em nosso site, você concorda que podemos coletar, armazenar e 
                                 utilizar os seus dados pessoais para os seguintes fins:
                             </span>
-                            <div style="margin-left: 3%;">
+                            <div style="margin-left: 3%; color: #717275"">
                                 <span>
                                     a. Envio de e-mails relacionados a atualizações do site, eventos e informações relevantes.
                                 </span><br>
@@ -59,7 +59,7 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">4.Segurança de Dados</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Empregamos medidas de segurança razoáveis para proteger os seus dados pessoais.
                                 No entanto, não podemos garantir a segurança completa dos dados transmitidos pela internet.
                             </span>
@@ -68,7 +68,7 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">5.Compartilhamento de Dados</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Não compartilharemos seus dados pessoais com terceiros sem o seu consentimento expresso,
                                 exceto quando necessário para cumprir obrigações legais.
                             </span>
@@ -77,7 +77,7 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">6.Alterações nos Termos</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Reservamo-nos o direito de modificar estes Termos a qualquer momento. Alterações significativas serão 
                                 notificadas através de nosso site ou por e-mail.
                             </span>
@@ -86,7 +86,7 @@
                     <li>
                         <span style="color: rgb(75, 75, 230);">7.Contato</span><br>
                         <div style="margin-left: 3%;">
-                            <span>
+                            <span style="color: #717275">
                                 Se você tiver alguma dúvida ou preocupação sobre estes Termos ou sobre 
                                 o uso de seus dados pessoais, entre em contato conosco através do seguinte 
                                 endereço de e-mail: <a href="mailto:secitec2023@gmail.com">secitec2023@gmail.com</a>.
