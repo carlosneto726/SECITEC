9cdbe9cf0462f3e8279819fc58262dba653e39db
carlosneto726 <carlosneto726@gmail.com>
2023-10-21 23:40:56 -0300

    retornando mensagens personalizadas do cadastramento dos eventos da monitoria

--- resources/views/monitoramento/view.blade.php
+++ resources/views/monitoramento/view.blade.php
@@ -2,40 +2,55 @@
 @section('content')
 
 
-<div class="container">
-    <div class="d-flex m-5">
-        <img class="mx-auto img-fluid" src="https://media.tenor.com/n4Js74XTN_4AAAAd/bh187-braveheart.gif">
-    </div>
-    
-    <h4 class="text-center">Página de cadastramento dos monitores, LETSGO CAMBADAA!!!</h4>
+<div class="container p-5">    
+    <h4 class="text-center">Página de cadastramento dos monitores</h4>
     <h5 class="text-center">APERTE NOS BOTÕES DO ABAIXO PARA SE CADASTRAR COMO MONITOR DO SEU TURNO</h5>
-    <div class="text-center">Se cadastre apenas nos turnos em que você foi selecionado, está com duvida? consulte a <a href="https://docs.google.com/spreadsheets/d/1sp7KI_g77PCA7qisBBlFyiW-pRHM74U9jyVtpyZH1CY/edit#gid=2137755893" target="_blank">planilha</a>.</div>
+    <div class="text-center">Se cadastre apenas nos turnos em que você foi selecionado, está com duvida? consulte a <a class="btn btn-outline-success btn-sm" href="https://docs.google.com/spreadsheets/d/1sp7KI_g77PCA7qisBBlFyiW-pRHM74U9jyVtpyZH1CY/edit#gid=2137755893" target="_blank">planilha</a></div>
     
     <div class="d-flex">
-        <form action="{{url('/usuarios/cadastarEvento')}}" method="post" class="m-5 mx-auto">
-            @method('POST')
-            @csrf
-            <input type="text" name="usuarioId" value="{{$usuario->id}}" hidden>
-            <input type="text" name="eventoId" value="80" hidden>
-            <button class="btn btn-primary btn-lg">Sou do turno da manhã</button>
-        </form>
-
-        <form action="{{url('/usuarios/cadastarEvento')}}" method="post" class="m-5 mx-auto">
-            @method('POST')
-            @csrf
-            <input type="text" name="usuarioId" value="{{$usuario->id}}" hidden>
-            <input type="text" name="eventoId" value="83" hidden>
-            <button class="btn btn-warning btn-lg">Sou do turno da tarde</button>
-        </form>
-
-        <form action="{{url('/usuarios/cadastarEvento')}}" method="post" class="m-5 mx-auto">
-            @method('POST')
-            @csrf
-            <input type="text" name="usuarioId" value="{{$usuario->id}}" hidden>
-            <input type="text" name="eventoId" value="84" hidden>
-            <button class="btn btn-secondary btn-lg">Sou do turno da noite</button>
-        </form>
+        <button class="btn btn-primary btn-lg m-5 mx-auto" onclick="cadastrar({{$usuario->id}}, 80)">Sou do turno da manhã</button>
+        <button class="btn btn-warning btn-lg m-5 mx-auto" onclick="cadastrar({{$usuario->id}}, 83)">Sou do turno da tarde</button>
+        <button class="btn btn-secondary btn-lg m-5 mx-auto" onclick="cadastrar({{$usuario->id}}, 84)">Sou do turno da noite</button>
     </div>
 </div>
 
+<script>
+
+function cadastrarMonitoriaPOST(endpoint, data){
+    var url = window.location.origin + endpoint; // Dominio + endpoint da API
+    var dados = data; // Dados no formato JSON
+    const configuracao = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: JSON.stringify(dados)
+    };
+
+    fetch(url, configuracao)
+    .then(resposta => resposta.json())
+    .then(data => {
+        if(data["mensagem"] == "cadastroNormal"){
+            alert("Cadastrado com sucesso.");
+        }else if(data["mensagem"] == "descadastroSemFila"){
+            alert("Descadastrado.");
+        }else{
+            alert("ERRO.");
+        }
+    })
+    .catch(erro => console.error('Erro:', erro));
+}
+
+function cadastrar(id_usuario, id_evento){
+    var endpoint = '/usuarios/cadastarEvento';
+    var data = {
+        usuarioId: id_usuario,
+        eventoId: id_evento
+    }
+
+    cadastrarMonitoriaPOST(endpoint, data);
+}
+
+</script>
 @endsection
