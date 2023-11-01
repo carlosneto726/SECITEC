1871626a7d34dcec5f406744aa2ac79753a7bc29
carlosneto726 <carlosneto726@gmail.com>
2023-11-01 16:58:49 -0300

    atualizando o gitignores

--- app/Console/Commands/GerarPDFsOrganizacao.php
+++ app/Console/Commands/GerarPDFsOrganizacao.php
@@ -31,14 +31,130 @@ class GerarPDFsOrganizacao extends Command
 
         $organizador = array(
             [
-                'nome' => 'Carlos Henrique Teixeira de Carvalho Neto',
-                'horas' => 80
+            'nome' => 'Aline da Conceição Lopes Braz',
+            'horas' => 60
             ],
 
+            [
+            'nome' => 'Alice Yasmim Santos Dourado',
+            'horas' => 20
+            ],
 
+            [
+            'nome' => 'Ana Clara Monteiro de Andrade',
+            'horas' => 40
+            ],
 
+            [
+            'nome' => 'Andressa Carvalho Vieira da Silva',
+            'horas' => 20
+            ],
 
-            
+            [
+            'nome' => 'Bruno Calazans Carritilha',
+            'horas' => 10
+            ],
+
+            [
+            'nome' => 'Carlos Henrique Teixeira de Carvalho Neto',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Deivison Barbosa do Nascimento',
+            'horas' => 40
+            ],
+
+            [
+            'nome' => 'Eliane Fernandes Pereira da Cruz',
+            'horas' => 10
+            ],
+
+            [
+            'nome' => 'Erik Takeshi Miura',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Flávia Espindula dos Santos',
+            'horas' => 40
+            ],
+
+            [
+            'nome' => 'Hendrew Neres de Queiroz',
+            'horas' => 20
+            ],
+
+            [
+            'nome' => 'Henrique Anthony Carneiro Silva',
+            'horas' => 20
+            ],
+
+            [
+            'nome' => 'Ícaro Maia Mendonça',
+            'horas' => 40
+            ],
+
+            [
+            'nome' => 'Jamily Luiza Marques Lourenço',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'João Marcos de Sousa Pieniz',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Juciely Sivirino Vieira',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Kelly Cristina da Silva Leite',
+            'horas' => 20
+            ],
+
+            [
+            'nome' => 'Letícia Bittencourt Vieira',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Leandro Sousa dos Santos',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Luciano Jose Vieira',
+            'horas' => 20
+            ],
+
+            [
+            'nome' => 'Matheus Costa Gonçalves',
+            'horas' => 20
+            ],
+
+            [
+            'nome' => 'Matheus Damacena Carvalho',
+            'horas' => 20
+            ],
+
+            [
+            'nome' => 'Maycon Douglas Dias da Silva',
+            'horas' => 60
+            ],
+
+            [
+            'nome' => 'Naoki Rafael Miura',
+            'horas' => 40
+            ],
+
+            [
+            'nome' => 'Sthéfanny Mémore do Carmo',
+            'horas' => 40
+            ]
+
         );
 
         foreach($organizador as $dado){
@@ -46,7 +162,6 @@ class GerarPDFsOrganizacao extends Command
 
             $nome = $dado['nome'];
             $horas = $dado['horas'];
-
 
             $pdf->AddPage();
             $imagePath = 'public/images/Certificado_frente.png';
@@ -70,7 +185,7 @@ class GerarPDFsOrganizacao extends Command
             $pdf->SetTextColor(0, 0, 0); // Cor preta (R, G, B)
             $pdf->SetFont('Arial','',20);
             
-            $pdf->MultiCell(0,20,utf8_decode("participou ativamente na criação e desenvolvimento do sistema de divulgação e credenciamento para a Semana de Educação, Ciência e Tecnologia (SECITEC) 2023 do IFG Campus Formosa, com carga horária total de $horas hora(s)."),0,"C",false);
+            $pdf->MultiCell(0,20,utf8_decode("participou da comissão organizadora da Semana de Educação, Ciência e Tecnologia (SECITEC) 2023 do IFG Campus Formosa., com carga horária total de $horas hora(s)."),0,"C",false);
             
             $x1 = 0;
             $y1 = 475;
@@ -82,7 +197,7 @@ class GerarPDFsOrganizacao extends Command
             $pdf->SetY(485);
             $pdf->SetFont("","",12);
             $pdf->Cell(0,5,utf8_decode("Comprovação de autenticidade"),0,1,"L",false);
-            $pdf->Output("F","public/pdfs/organizadores/DESENVOLVEDORES/_".mb_strtoupper($nome)."_.pdf");
+            $pdf->Output("F","public/pdfs/organizadores/COMISSAO/_".mb_strtoupper($nome)."_.pdf");
         }
     }
 }
