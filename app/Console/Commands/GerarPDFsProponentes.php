afb5429876116ed5f60953748997b2863c678564
carlosneto726 <carlosneto726@gmail.com>
2023-11-02 00:42:21 -0300

    atualizando a importação do fpdf

--- app/Console/Commands/GerarPDFsProponentes.php
+++ app/Console/Commands/GerarPDFsProponentes.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use App\Fpdf\FPDF;
+use App\Fpdf\fpdf;
 
 class GerarPDFsProponentes extends Command
 {
