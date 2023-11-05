b20834a6a7ac1959d53c287c448d9a33163bbfa8
Juliano Marcelo Schaurich Júnior <schaurich.jmsj@gmail.com>
2023-11-05 17:42:08 -0300

    tentei mudar o botao de download do proponente tirando a cor e alinhando a direita, só nao consegui vir como ficou

--- resources/views/certificados/view.blade.php
+++ resources/views/certificados/view.blade.php
@@ -147,7 +147,7 @@
                                 @if(count($links) > 1)
 
                                 <div class="dropdown">
-                                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                                             <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                             <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
@@ -155,7 +155,7 @@
                                     </button>
                                     <ul class="dropdown-menu">
                                         @foreach ($links as $link)
-                                            <li><a class="dropdown-item" href="{{asset("pdfs/proponentes/".$link)}}" target="_blank">{{str_replace("_.pdf", "", explode("-", $link)[1])}}</a></li>
+                                            <li class="text-end"><a class="dropdown-item" href="{{asset("pdfs/proponentes/".$link)}}" target="_blank">{{str_replace("_.pdf", "", explode("-", $link)[1])}}</a></li>
                                         @endforeach
                                     </ul>
                                 </div>
@@ -263,7 +263,6 @@
         pesquisar();
     }
     var urlAtual = window.location.href;
-    console.log(urlAtual);
     if(urlAtual.includes("#")){
         selecionarSecao(0);
     }    
